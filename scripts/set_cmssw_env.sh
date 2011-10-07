506fdc1e14b4d748263d8bf11521b369bd7c5aea
Andrew Gilbert <ajgilbert@me.com>
2011-10-07 14:15:04 +0200

    Add some comments to the cmmsw env script

--- scripts/set_cmssw_env.sh
+++ scripts/set_cmssw_env.sh
@@ -1,3 +1,6 @@
+#This is only of use on the laptop at the moment. 
+#It will be updated when it is possible to install the
+#full cms framework.
 export SCRAM_ARCH=osx106_amd64_gcc421
 source ~/CMSSW_4_2_3_FWLITE/cmsset_default.sh
 cd ~/CMSSW_4_2_3_FWLITE/work/CMSSW_4_2_3_FWLITE/src/
