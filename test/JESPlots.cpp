338b4e5d32693d3de8959998908dadd3bbef0c9e
Andrew Gilbert <ajgilbert@me.com>
2012-02-16 00:18:07 +0100

    update

--- test/JESPlots.cpp
+++ test/JESPlots.cpp
@@ -39,24 +39,58 @@ int main(int argc, char* argv[]){
   filepaths.push_back("/data/Zb/TTJets/4_2_4/S4/TuneZ2/jes_minus_ee_PF_SSVHE.root");
   filepaths.push_back("/data/Zb/ZZ/4_2_4/S4/TuneZ2/jes_minus_ee_PF_SSVHE.root");
   
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/bFlav/jes_central_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/cFlav/jes_central_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/lFlav/jes_central_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/TTJets/4_2_4/S4/TuneZ2/jes_central_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/ZZ/4_2_4/S4/TuneZ2/jes_central_mm_PF_SSVHE.root");
+  
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/bFlav/jes_plus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/cFlav/jes_plus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/lFlav/jes_plus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/TTJets/4_2_4/S4/TuneZ2/jes_plus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/ZZ/4_2_4/S4/TuneZ2/jes_plus_mm_PF_SSVHE.root");
+  
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/bFlav/jes_minus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/cFlav/jes_minus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/DYJetsToLL/4_2_4/S4/TuneZ2/lFlav/jes_minus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/TTJets/4_2_4/S4/TuneZ2/jes_minus_mm_PF_SSVHE.root");
+  filepaths.push_back("/data/Zb/ZZ/4_2_4/S4/TuneZ2/jes_minus_mm_PF_SSVHE.root");
   std::vector<std::string> newname;
-  newname.push_back("DYbflav_central");
-  newname.push_back("DYcflav_central");
-  newname.push_back("DYlflav_central");
-  newname.push_back("TTJets_central");
-  newname.push_back("ZZ_central");
+  newname.push_back("ee_DYbflav_central");
+  newname.push_back("ee_DYcflav_central");
+  newname.push_back("ee_DYlflav_central");
+  newname.push_back("ee_TTJets_central");
+  newname.push_back("ee_ZZ_central");
 
-  newname.push_back("DYbflav_plus");
-  newname.push_back("DYcflav_plus");
-  newname.push_back("DYlflav_plus");
-  newname.push_back("TTJets_plus");
-  newname.push_back("ZZ_plus");
+  newname.push_back("ee_DYbflav_plus");
+  newname.push_back("ee_DYcflav_plus");
+  newname.push_back("ee_DYlflav_plus");
+  newname.push_back("ee_TTJets_plus");
+  newname.push_back("ee_ZZ_plus");
 
-  newname.push_back("DYbflav_minus");
-  newname.push_back("DYcflav_minus");
-  newname.push_back("DYlflav_minus");
-  newname.push_back("TTJets_minus");
-  newname.push_back("ZZ_minus");
+  newname.push_back("ee_DYbflav_minus");
+  newname.push_back("ee_DYcflav_minus");
+  newname.push_back("ee_DYlflav_minus");
+  newname.push_back("ee_TTJets_minus");
+  newname.push_back("ee_ZZ_minus");
+  newname.push_back("mm_DYbflav_central");
+  newname.push_back("mm_DYcflav_central");
+  newname.push_back("mm_DYlflav_central");
+  newname.push_back("mm_TTJets_central");
+  newname.push_back("mm_ZZ_central");
+
+  newname.push_back("mm_DYbflav_plus");
+  newname.push_back("mm_DYcflav_plus");
+  newname.push_back("mm_DYlflav_plus");
+  newname.push_back("mm_TTJets_plus");
+  newname.push_back("mm_ZZ_plus");
+
+  newname.push_back("mm_DYbflav_minus");
+  newname.push_back("mm_DYcflav_minus");
+  newname.push_back("mm_DYlflav_minus");
+  newname.push_back("mm_TTJets_minus");
+  newname.push_back("mm_ZZ_minus");
   TFile f((outfile+".root").c_str(),"RECREATE");
   for (unsigned i = 0; i < filepaths.size(); ++i) {
     TFile g(filepaths[i].c_str());
